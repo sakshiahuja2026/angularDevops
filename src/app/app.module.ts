ef3f3c6fe5bd6f124232178e18314920eea601a3
sakshiahuja2026@gmail.com <sakshiahuja20262gmail.com>
2022-08-22 15:50:04 +0530

    ang-dev

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from "@angular/forms"
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from "@angular/common/http";
+import { HomeComponent } from './home/home.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule
+  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { SessionService } from '../session.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  email = ""
+  password = ""
+
+  constructor(private sessionService: SessionService,private toastr:ToastrService,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    let user = { "email": this.email, "password": this.password }
+    this.sessionService.loginApi(user).subscribe(resp => {
+      this.toastr.success("Login done")
+      this.router.navigateByUrl("/home")
+    }, err => {
+      this.toastr.error("Invalid Credentials....","401")
+    })
+  }
+
+
+  //Bcrypt
+  //@Transcational 
+
+}
